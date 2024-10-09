d5a0dec09ad530f2ebef49460a1f48677aa39199
Allofme2025 <yolattanavong789@gmail.com>
2024-10-09 14:23:28 +0700

    update

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -38,7 +38,7 @@ export default async function LatestInvoices() { // Remove props
                       {invoice.name}
                     </p>
                     <p className="hidden text-sm text-gray-500 sm:block">
-                      {invoice.email}
+                      {invoice.phone}
                     </p>
                   </div>
                 </div>
